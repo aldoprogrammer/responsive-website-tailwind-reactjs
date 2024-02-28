a47e00dfba3b05f3223e16bd44173be9b3d73910
aldoprogrammer <aldobesma@gmail.com>
2024-02-28 18:47:55 +0700

    update

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import styles, { layout } from '../style'
+import { features } from '../constants'
+import Button from './Button'
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id='features' className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Code the web application, <br className='sm:block hidden'/>
+          using React JS.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, tenetur commodi. Facere illo fugiat, culpa ut, praesentium placeat qui quis mollitia vel ipsa fugit! Quibusdam ut a maiores nisi? Rem.</p>
+        </div>
+    </section>
   )
 }
 

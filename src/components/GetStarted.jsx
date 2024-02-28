463004dc9eb55cc5537c99bc8499d3a99dd119cc
aldoprogrammer <aldobesma@gmail.com>
2024-02-28 17:23:36 +0700

    update

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -8,13 +8,23 @@ const GetStarted = () => (
     <div className={`${styles.flexCenter} flex-col bg-primary
     w-[100%] h-[100%] rounded-full`}>
       <div className={`${styles.flexStart} flex-row`}>
-        <div className="font-poppins text-lg font-medium
-        leading-[23px]">
+        <p className="font-poppins text-lg font-medium
+        leading-[23px] mr-2">
           <span className="text-gradient">
             Get
           </span>
-        </div>
+        </p>
+        <img src={arrowUp} alt="arrow" 
+          className='w-[23px] h-[23px] object-contain'
+          />
       </div>
+
+      <p className="font-poppins text-lg font-medium
+        leading-[23px]">
+          <span className="text-gradient">
+            Started
+          </span>
+        </p>
     </div>
   </div>
 )

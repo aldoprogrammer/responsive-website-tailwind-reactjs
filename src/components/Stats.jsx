a8f5e026a4b2ed1544e1c3b1190f89628018280e
aldoprogrammer <aldobesma@gmail.com>
2024-02-28 18:42:34 +0700

    update

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -6,9 +6,10 @@ const Stats = () => (
   <section className={`${styles.flexCenter} 
   flex-row flex-wrap sm:mb-20 mb-6}`}>
     {stats.map((stat) => (
-      <div key={stat.id} className={`flex-1`}>
+      <div key={stat.id} className={`flex-1 flex justify-start items-center
+      flex-row m-3`}>
         <h4 className='font-poppins font-semibold
-        xs:text-[40px] text-[30px] 
+        xs:text-[40px] text-[30px] mr-2
         xs:leading-[53px] leading-[43px] text-white'>{stat.value}</h4>
         <p className='font-poppins font-normal
         xs:text-[20px] text-[15px] 

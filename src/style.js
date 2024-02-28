f63f51280e183204d1543ad16e5d75a6a4715bb9
aldoprogrammer <aldobesma@gmail.com>
2024-02-28 23:08:10 +0700

    update

--- src/style.js
+++ src/style.js
@@ -20,7 +20,7 @@ export const layout = {
   sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
 
   sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
-  sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
+  sectionImg: `flex-1 flex-col ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
 
   sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
 };

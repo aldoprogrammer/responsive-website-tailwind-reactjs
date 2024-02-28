6b96f3f8b0262383a27aabf4e1306e0d47a5e5d5
aldoprogrammer <aldobesma@gmail.com>
2024-02-28 17:06:44 +0700

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
   return (
     <nav className='w-full flex py-6 justify-between
     items-center navbar'>
-      <img src={logo} alt="" 
-      className='w-[124px] h-[32px]'/>
+      <img src={logo} alt=""
+        className='w-[124px] h-[32px]' />
       <ul className='list-none sm:flex hidden justify-end items-center
       flex-1'>
         {navLinks.map((nav, index) => (
@@ -30,10 +30,34 @@ const Navbar = () => {
 
       <div className='sm:hidden flex flex-1
       justify-end items-center'>
-        <img src={toggle ? close : menu } alt="menu" 
-        className='w-[28px] h-[28px] object-contain'
-        onClick={() => setToggle((prev) => !prev)}
+        <img src={toggle ? close : menu} alt="menu"
+          className='w-[28px] h-[28px] object-contain'
+          onClick={() => setToggle((prev) => !prev)}
         />
+
+        <div className={`${toggle ? 'flex' : 'hidden'} p-6
+        bg-black-gradient absolute top-20 right-0
+         mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+          <ul className='list-none flex flex-col
+          justify-end items-center
+          flex-1'>
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal
+            cursor-pointer text-base text-white
+            ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'}
+            `}
+              >
+                <a href={`#${nav.id}`}
+                >
+                  {nav.title}
+                </a>
+
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   )

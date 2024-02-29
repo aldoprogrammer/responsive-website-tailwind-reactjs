26c1d1b1008f93b3b573ccc2e33537140d0bc3ff
aldoprogrammer <aldobesma@gmail.com>
2024-02-29 11:09:36 +0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => (
 
         <div className='w-full flex justify-between items-center md:flex-row flex-col border-t-[1px] border-t[#3F3E45] pt-5'>
             <p className='font-poppins font-normal text-lg leading-[27px] text-white'>
-                Copyright © 2024 <a href="https://github.com/aldoprogrammer">Aldo L.S.</a> All Rights Reserved.
+                Copyright © 2024 <a href="https://github.com/aldoprogrammer">Aldo Lata Soba</a> All Rights Reserved.
             </p>
             <div className='flex flex-row md:mt-0 mt-6'>
                 {socialMedia.map((social, index) => (

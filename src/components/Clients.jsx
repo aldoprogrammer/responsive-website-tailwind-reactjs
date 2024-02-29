8c87cf23a1cb7d12167d4f6f0001d153b8ef39b8
aldoprogrammer <aldobesma@gmail.com>
2024-02-29 09:20:27 +0700

    update

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { clients } from '../constants';
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+const Clients = () => (
+  <section className="flex justify-center my-4">
+    <div className="flex flex-wrap w-full">
+      {clients.map((client) => (
+        <div key={client.id} className="flex-1 flex justify-center sm:min-w-[192px] min-w-[120px]">
+          <img
+            src={client.logo}
+            alt="client"
+            className="sm:w-[192px] w-[100px] object-contain hover:filter hover:brightness-150 hover:invert-100 cursor-pointer"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
-export default Clients
+export default Clients;
